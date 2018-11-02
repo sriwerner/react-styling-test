6c66d5200ffc51026c60733dbb8571c0b2f9c550
manuel <manuel@sahores.com>
2018-11-02 20:04:51 -0300

    person added, jsx media queries with radium

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
+import Person from './components/Person/Person';
 
 class App extends Component {
   state = {
     textVisible: true,
+    showPersons: true,
+    persons: [{
+      name: 'Max',
+      age: 32,
+      id: 'alskdjfbn0897'
+    }, {
+      name: 'Anex',
+      age: 74,
+      id: 'lkajsnbdf9p8qhw34pkjb'
+    }]
   }
   
   onClickHandler = () => {
     this.setState({textVisible: !this.state.textVisible})
   }
+
+  deletePersonHandler = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({persons: persons})
+  }
+
+  nameChangeHandler = (event, person) => {
+    // this.persons.findById
+  }
   
   render() {
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person name={person.name} age={person.age} key={person.id} click={() => this.deletePersonHandler(index)} changed={(event)=>this.nameChangeHandler(event, person)}/>
+          })}
+        </div>
+      )
+    }
 
     let text = "Hola :D";
     const style = {
@@ -46,11 +79,14 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <Button text='Dale masa' click={()=>this.onClickHandler()} style={btnClasses.map(it=>it)}/>
-        <h1>{text}</h1>
-        <div style={style}></div>
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <Button text='Dale masa' click={()=>this.onClickHandler()} style={btnClasses.map(it=>it)}/>
+          <h1>{text}</h1>
+          <div style={style}></div>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
